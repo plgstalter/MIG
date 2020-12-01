72720059f2cc1f4623208fef1d2ba36af1f36aa9
apierfitte <auguste.pierfitte@gmail.com>
2020-12-01 16:55:16 +0100

    ajout du programme add-user.py pour ajouter patients et médecins à la base de données

--- Code/add_user.py
+++ Code/add_user.py
@@ -1,7 +1,39 @@
-import sys, sqlite3
+import sqlite3
+import creation_page
 
-email, password = sys.argv[1:]
+def ajout_medecin(secu, nom, prenom, mdp, adresse, numero, mail):
+    conn = sqlite3.connect('../Ressources/Donnees/flowmed.db')
+    cur = conn.cursor()
 
-source = open(squelette.html, 'r')
+    params = (secu, nom, prenom, mdp, "'" + adresse + "'", numero, "'" + mail + "'")
 
-#a completer
+    sqlite_insert_query = "INSERT INTO medecins VALUES (?,?,?,?,?,?,?)"
+
+    cur.execute(sqlite_insert_query, params)
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    creation_page.medecin(nom, prenom, adresse, mail, numero)
+
+
+def ajout_patient(secu, nom, prenom, naissance, mdp, adresse, mail, numero):
+    '''
+    ajoute un patient dans la bdd, par défaut, son compte n'est pas activé
+    '''
+
+    conn = sqlite3.connect('../Ressources/Donnees/flowmed.db')
+    cur = conn.cursor()
+
+    params = (secu, nom, prenom, naissance, mdp, "'" + adresse + "'", "'" + mail + "'", numero)
+
+    sqlite_insert_query = "INSERT INTO medecins VALUES (?,?,?,?,?,?,?,?)"
+
+    cur.execute(sqlite_insert_query, params)
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    creation_page.patient(nom, prenom, naissance, mail, mdp, secu)
