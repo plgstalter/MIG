1460d04c0259758f81d2a769a0de408bc83497a5
apierfitte <auguste.pierfitte@gmail.com>
2020-12-02 22:15:11 +0100

    nettoyage de la bdd

---
+++ Ressources/Pages/info.269054958815780.php
@@ -0,0 +1,49 @@
+<DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8"/>
+            <title>Informations personnelles</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <h1>FlowMed, le futur de la médecine</h1>
+            <div class="div1" style="height:400px">
+                <h2>Espace patient</h2>
+                <h3>Mes informations personnelles</h3>
+                
+                <br />
+                <table align=center width = "20%" border = "1" bordercolor = "grey" style="text-align: center; background-color: lightsteelblue;">
+                    <tr>
+                        <td>Nathalie</td></tr>
+                    <tr>
+                        <td>Durand</td></tr>
+                    <tr>
+                        <td>pauline.delarue@mines-paristech.fr</td></tr>
+                    <tr>
+                        <td>0789415632</td></tr>
+                    <tr>    
+                        <td>60 boulevard Saint-Michel</td></tr>
+                    <tr>   
+                        <td>05/04/1969</td></tr>
+                    <tr>
+                        <td>Femme</td></tr>
+                    <tr>
+                        <td>269054958815780</td></tr>
+        
+                </table>
+
+                <div style="width:1000px;">
+                        <p style="text-align: center;">
+                            <a href="">
+                                Modifier mes informations
+                            </a>
+                        </p>
+                        <p style="text-align:center">
+                            <a href="">
+                                Changer mon mot de passe
+                            </a>
+                        </p>
+                </div>
+            </div>
+        </body>
+    </html>
---
+++ Ressources/Pages/medecin.171564898754564.php
@@ -0,0 +1,45 @@
+<DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8"/>
+            <title>Espace Médecin</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <h1>FlowMed, le futur de la médecine</h1>
+            
+            <div class="div1" style="height:350px">
+            <h2>Espace médecin</h2>
+                <p>
+                    Bienvenue sur votre espace, Michel !
+                </p>
+                
+                <br />
+                <div style="width:1000px; background-color: rgb(255, 222, 209)">
+                    <div class = "div3" style="float:left;">
+                        <p> 
+                            <a href="">Les alertes de mes patients</a>
+                        </p>
+                    </div>
+                    <div class = "div3" style="float:left; text-align:left; float:right;">
+                        <p>
+                            Dupont Michel <br/>
+                            270 rue Saint-Jacques <br />
+                            paul.guillot@mines-paristech.fr <br />
+                            0678945815 <br />
+                        </p>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div class = "div3" style="float:left;">
+                        <p> 
+                            <a href="">Mes patients</a>
+                        </p>
+                    </div>
+                </div>
+        </div>
+    </body>
+</html
