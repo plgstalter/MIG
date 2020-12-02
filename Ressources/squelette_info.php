c3a5fcfe314cc9acf41097c3587a514654542b74
Paul Guillot <paul.guillot@mines-paristech.fr>
2020-12-02 15:46:21 +0100

    modifs 3 squelettes

---
+++ Ressources/squelette_info.php
@@ -0,0 +1,48 @@
+<DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8"/>
+            <title>Informations personnelles</title>
+        </head>
+        <body>
+            <h1>FlowMed, le futur de la médecine</h1>
+            <h2>Espace patient</h2>
+            <h3>Informations personnelles</h3>
+            Bienvenue sur votre espace, $prenom$ !
+            
+            <br />
+            <br />
+            <table align = width = "50%" border = "1" bordercolor = "grey">
+                <tr>
+                    <td>$prenom$</td></tr>
+                <tr>
+                    <td>$nom$</td></tr>
+                <tr>
+                    <td>$mail$</td></tr>
+                <tr>
+                    <td>$numero$</td></tr>
+                <tr>    
+                    <td>$adresse$</td></tr>
+                <tr>   
+                    <td>$naissance$</td></tr>
+                <tr>
+                    <td>$sexe$</td></tr>
+                <tr>
+                    <td>$secu$</td></tr>
+    
+            </table>
+
+            <div style="width:1000px;">
+                    <p>
+                        <a href="">
+                            Modifier mes informations
+                        </a>
+                    </p>
+                    <p>
+                        <a href="">
+                            Changer mon mot de passe
+                        </a>
+                    </p>
+            </div>
+        </body>
+    </html>
--- Ressources/squelette_patient.php
+++ Ressources/squelette_patient.php
@@ -13,22 +13,23 @@
             <br />
             <br />
             <div style="width:1000px;">
-                <div style="width:200px; height:50px; text-align:center; border: solid 1px #333333; float:left;">
+                <div style="width:200px; height:50px; text-align:center; vertical-align:middle ; border: solid 1px #333333; float:left;">
                     <p>
                         <a href="">Mes questionnaires</a>
                     </p>
                 </div>
-                <div style="width:300px; height:100px; border: solid 1px #333333; float:right;">
-                    <br/>
+                <div style="width:300px; height:100px; vertical-align:middle; border: solid 1px #333333; float:right;">
+                    <p>
                     $nom$ $prenom$ <br /> 
                     $mail$ <br />
                     Né le : $date$ <br />
+                    </p>
                 </div>
                 <br />
                 <br />
                 <br />
                 <br />
-                <div style="width:200px; height:50px; text-align:center; border: solid 1px #333333; float:left;">
+                <div style="width:200px; height:50px; text-align:center; vertical-align:middle; border: solid 1px #333333; float:left;">
                     <p>
                         <a href="">Mon suivi</a>
                     </p>
