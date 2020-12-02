e218ab00923c5bdd198f4e9d282cd3a7bf119645
Paul Guillot <paul.guillot@mines-paristech.fr>
2020-12-02 17:49:26 +0100

    debut css

--- Ressources/squelette_patient.php
+++ Ressources/squelette_patient.php
@@ -4,35 +4,48 @@
         <head>
             <meta charset="utf-8"/>
             <title>Espace Patient</title>
+            <link rel="stylesheet" href="style.css">
         </head>
         <body>
             <h1>FlowMed, le futur de la médecine</h1>
-            <h2>Espace patient</h2>
-            Bienvenue sur votre espace, $prenom$ !
-    
-            <br />
-            <br />
-            <div style="width:1000px;">
-                <div style="width:200px; height:50px; text-align:center; vertical-align:middle ; border: solid 1px #333333; float:left;">
-                    <p>
-                        <a href="">Mes questionnaires</a>
-                    </p>
-                </div>
-                <div style="width:300px; height:100px; vertical-align:middle; border: solid 1px #333333; float:right;">
-                    <p>
-                    $nom$ $prenom$ <br /> 
-                    $mail$ <br />
-                    Né le : $date$ <br />
-                    </p>
-                </div>
+            
+            <div class="div1" style="height:350px">
+                <h2>Espace patient</h2>
+                <p>Bienvenue sur votre espace, $prenom$ !</p>
+        
                 <br />
                 <br />
-                <br />
-                <br />
-                <div style="width:200px; height:50px; text-align:center; vertical-align:middle; border: solid 1px #333333; float:left;">
-                    <p>
-                        <a href="">Mon suivi</a>
-                    </p>
+                <div style="width:1000px; border:1px; background-color:lightblue; background-position: center;">
+                    <div class = "div3" style="float:left;">
+                        <p>
+                            <a href="">Mes questionnaires</a>
+                        </p>
+                    </div>
+                    <div class = "div3" style="text-align:left;float:right">
+                        <p>
+                        $nom$ $prenom$ <br /> 
+                        $mail$ <br />
+                        Né le : $date$ <br />
+                        </p>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div class = "div3" style="float:left;">
+                        <p>
+                            <a href="">Mon suivi</a>
+                        </p>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div class = "div3" style=" float:left;">
+                        <p>
+                            <a href="">Mes informations personnelles</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </body>
