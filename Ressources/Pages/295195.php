97b2a70e9f15b36d5a8787c9de439f22bc3be06a
apierfitte <auguste.pierfitte@gmail.com>
2020-12-02 09:25:28 +0100

    ajout de Nath Durand pour la demo

---
+++ Ressources/Pages/295195.php
@@ -0,0 +1,51 @@
+
+<DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="utf-8"/>
+            <title>Espace Patient</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <h1>FlowMed, le futur de la médecine</h1>
+            
+            <div class="div1" style="height:350px">
+                <h2>Espace patient</h2>
+                <p>Bienvenue sur votre espace, Nathalie !</p>
+        
+                <br />
+                <div style="width:1000px; border:1px; background-color:lightblue; background-position: center;">
+                    <div class = "div3" style="float:left;">
+                        <p>
+                            <a href="">Mes questionnaires</a>
+                        </p>
+                    </div>
+                    <div class = "div3" style="text-align:left;float:right">
+                        <p>
+                        Durand Nathalie <br /> 
+                        pierre-louis.gstalter@mines-paristech.fr <br />
+                        Né le : 15/02/2012 <br />
+                        </p>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div class = "div3" style="float:left;">
+                        <p>
+                            <a href="">Mon suivi</a>
+                        </p>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div class = "div3" style=" float:left;">
+                        <p>
+                            <a href="">Mes informations personnelles</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html
