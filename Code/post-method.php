a6b5ef31c43a2751c381f0b432c73e077c8c5c5c
plgstalter <pierrelouisgstalter67@gmail.com>
2020-12-03 16:10:34 +0100

    debug

--- Code/post-method.php
+++ Code/post-method.php
@@ -1,5 +1,5 @@
 <?php
-require "session.php";
+session_start();
 $_SESSION['compteur'] = 0; //compteur pour voir le nombre d'essais
 // if ($_SESSION['compteur'] > 9) {
 //     header("wrong.html");
@@ -9,7 +9,7 @@ if ($_REQUEST['mail'] == '') {
     header('accueil.php');
     exit;
 }
-$_SESSION['mail'] = $_REQUEST['mail'];
+$_SESSION['mail'] = "'".$_REQUEST['mail']."'";
 $flowmed = new SQLite3('../Ressources/Donnees/flowmed.db');  // introduction de la base de données
 $statement = $flowmed -> prepare("SELECT secu, mdp FROM patients WHERE mail = :imaginary_word");
 $statement -> bindValue(':imaginary_word', $_SESSION['mail']);
@@ -17,15 +17,15 @@ $result = $statement -> execute();
 while ($row = $result->fetchArray()) {
     $_SESSION['secu'] = $row['secu'];
     $_SESSION['mdp'] = $row['mdp'];
-    $url = '../Ressources/Pages/'.$_SESSION['mdp'].'.php';
+    $url = '../Ressources/Pages/'.$_SESSION['secu'].'.php';
 }
 if ($_REQUEST['mdp'] == $_SESSION['mdp']) {
     $_SESSION['connecte'] = TRUE;
-    echo('location: '.$url); // si on est bon, on envoie sur la page du patient
+    header('location: '.$url); // si on est bon, on envoie sur la page du patient
 }
 else {
     $_SESSION['compteur']++;
-    header("accueil.php"); // on réessaye de se connecter
+    header("location: accueil.php"); // on réessaye de se connecter
 }
 $flowmed = null; // on se sépare de la base de données à la fin de la page
 ?>
