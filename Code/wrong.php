c3236c50f0efe88792a08875c44fda138b8f72b9
plgstalter <43704583+plgstalter@users.noreply.github.com>
2020-12-03 09:08:26 +0100

    Delete wrong.php
    la version html est amplement suffisante

--- Code/wrong.php
+++
@@ -1,9 +0,0 @@
-<DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8"/>
-        <title> FlowMed </title>
-    </head>
-    <body>
-        Erreur ! Mauvais email/mot de passe !
-    </body>
