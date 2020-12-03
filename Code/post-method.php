67bcba2d2490446f18e8cf8ed2d9b1fe9a9e9e36
plgstalter <pierrelouisgstalter67@gmail.com>
2020-12-03 07:54:17 +0100

    page dynamique OK, il ne reste qu'à traiter le cas du mot de passe faux

--- Code/post-method.php
+++ Code/post-method.php
@@ -1,13 +1,83 @@
 <?php
 $email = $_REQUEST['email'];
-$p2 = $_REQUEST['password'];
+$password = $_REQUEST['password'];
 $flowmed = new SQLite3('../Ressources/Donnees/flowmed.db');  // introduction de la base de données
-// $result = $flowmed->QUERY('SELECT secu FROM patients WHERE mail ='.$email);
-$statement = $flowmed -> prepare("SELECT secu FROM patients WHERE secu = :imaginary_word");
-$statement -> bindValue(':imaginary_word', 598495);
+$statement = $flowmed -> prepare("SELECT secu, mdp FROM patients WHERE mail = :imaginary_word");
+$statement -> bindValue(':imaginary_word', $email);
 $result = $statement -> execute();
 while ($row = $result->fetchArray()) {
-    $x = $row['secu'];
+    $secu = $row['secu'];
+    $mdp = $row['mdp'];
 }
-echo "<a href=../Pages/".$x.".php> votre session </a>";
+
+if ($mdp == $password) {
+    session_start();
+    $statement_2 = $flowmed -> prepare("SELECT * FROM patients WHERE secu = :imaginary_word");
+    $statement_2 -> bindValue(':imaginary_word', $secu);
+    $result_2 = $statement_2 -> execute();
+    while ($row = $result_2->fetchArray()) {
+        $nom = $row['nom'];
+        $prenom = $row['prenom'];
+        $naissance = $row['naissance'];
+        $adresse = $row['adresse'];
+    }
+}
+else {
+    // erreur : accès à la page interdit ! 
+}
 ?>
+
+<DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Espace Patient</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>FlowMed, le futur de la médecine</h1>
+        
+        <div class="div1" style="height:350px">
+            <h2>Espace patient</h2>
+            <?php
+            echo "<p>Bienvenue sur votre espace, ".$prenom." !</p>";
+            ?>
+    
+            <br />
+            <div style="width:1000px; border:1px; background-color:lightblue; background-position: center;">
+                <div class = "div3" style="float:left;">
+                    <p>
+                        <a href="">Mes questionnaires</a>
+                    </p>
+                </div>
+                <div class = "div3" style="text-align:left;float:right">
+                    <p>
+                    <?php
+                    echo $nom, $prenom." <br />"; 
+                    echo $email." <br />";
+                    echo "Né le : ".$naissance." <br />";
+                    ?>
+                    </p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <div class = "div3" style="float:left;">
+                    <p>
+                        <a href="">Mon suivi</a>
+                    </p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <div class = "div3" style=" float:left;">
+                    <p>
+                        <a href="">Mes informations personnelles</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
