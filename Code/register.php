9790d30357945434c159e51f1ff8e582df183fcb
plgstalter <pierrelouisgstalter67@gmail.com>
2020-12-03 11:07:23 +0100

    création page s'inscrire

--- Code/register.php
+++ Code/register.php
@@ -1,6 +1,3 @@
-<?php
-session_start();
-?>
 <DOCTYPE html>
 <html lang="fr">
     <head>
@@ -11,14 +8,14 @@ session_start();
         ?>
     </head>
     <body>
-        <h1>
+    <h1>
             FlowMed, le futur de la médecine
         </h1>
         <p>
-            Bienvenue sur FlowMed ! Pour accéder à la plateforme en ligne, veuillez vous identifier.
+            Bienvenue sur FlowMed ! Pour vous inscrire, entrez votre email et créez un mot de passe.
         </p>
         <div>
-            <div style = "width:350px; border: solid 1px #333333; " align = "left">
+            <div style = "width:400px; border: solid 1px #333333; " align = "left">
                 <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
                     
                 <div style = "margin:30px" align="right">
@@ -27,27 +24,15 @@ session_start();
                         echo "Erreur dans vos entrées. Veuillez réessayer. <br />";
                     }
                     ?>
-                    <form action = "post-method.php" method = "post">
-                    <!-- Je suis ... 
-                    <input type="checkbox" name="personne[]" value="medecin">
-                    <label for="personne_1"> médecin </label>
-                    <input type="checkbox" name="personne[]" value="patient">
-                    <label for="personne_2"> patient </label><br> -->
+                    <form action = "post-register.php" method = "post">
                     <label>e-mail :      </label><input type = "text" name = "mail" class = "box"/><br /><br />
                     <label>mot de passe :</label><input type = "password" name = "mdp" class = "box" /><br/><br />
+                    <label>confirmer le mot de passe :</label><input type = "password" name = "mdp" class = "box" /><br/><br />
                     <input type = "submit" value = " Submit "/><br />
-                    </form>  
-                    
-                    Pas encore de compte ? <a href="register.php"> S'inscire </a>
-                    
-                        
+                    </form>    
                 </div>
                     
             </div>
                 
         </div>
     </body>
-    <?php
-    $flowmed = null; // on se sépare de la base de données à la fin
-    ?>
-</html>
